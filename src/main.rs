b4eeeb905fcbe37d11fdbb651a7a741c82927c43
Bob Mannino <manninobobby@icloud.com>
2025-08-13 20:48:50 +0100

    refactor: change to use TodoList struct

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,43 @@
 mod todo;
-mod todos;
 
-use crate::todos::{get_all_todos, list_todos, save_todos};
-use std::{env, process};
+use crate::todo::{Todo, TodoList};
+use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let mut todos = get_all_todos().expect("Failed to get todos");
+    let mut todo_list = TodoList::new().expect("Failed to get todos");
 
     if args.len() == 1 {
         print!("Use one of the commands to see something happen");
     } else if args.len() == 2 {
         if args[1] == "list" {
-            let mut todos = todos.clone();
-            todos.retain(|todo| !todo.is_done());
-            list_todos(&todos);
+            todo_list.todos_mut().retain(|todo| !todo.is_done());
+            todo_list.list();
         } else if args[1] == "list-done" {
-            let mut todos = todos.clone();
-            todos.retain(|todo| todo.is_done());
-            list_todos(&todos);
+            todo_list.todos_mut().retain(|todo| todo.is_done());
+            todo_list.list();
         } else if args[1] == "list-all" {
-            list_todos(&todos);
+            todo_list.list();
         }
     } else if args.len() == 3 {
         if args[1] == "done" {
             let todo_id: u32 = args[2].parse().expect("Invalid todo ID");
-            let todo = todos.iter_mut().find(|todo| todo.id() == todo_id);
+            let todo = todo_list
+                .todos_mut()
+                .iter_mut()
+                .find(|todo| todo.id() == todo_id);
 
             if let Some(todo) = todo {
                 todo.toggle_done();
-                save_todos(&todos);
+                todo_list.save();
             } else {
                 println!("No todo with that ID")
             }
         } else if args[1] == "remove" {
             let todo_id: u32 = args[2].parse().expect("Invalid todo ID");
-            todos.retain(|todo| todo.id() != todo_id);
-            save_todos(&todos);
+            todo_list.todos_mut().retain(|todo| todo.id() != todo_id);
+            todo_list.save();
         }
     }
 }
--- src/todo.rs
+++ src/todo.rs
@@ -1,5 +1,58 @@
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+use std::{
+    fs::File,
+    io::{self, Read, Write},
+};
+
+pub struct TodoList {
+    todos: Vec<Todo>,
+}
+
+impl TodoList {
+    /// Reads todos from the "todos.json" file and returns them as a Vec<Todo>. If
+    /// the file does not exist it will create an empty one. Sorted by date created
+    /// ascending
+    pub fn new() -> Result<Self, io::Error> {
+        let mut todos_str = String::new();
+
+        if !File::open("todos.json").is_ok() {
+            File::create("todos.json")?.write_all(b"[]")?;
+        }
+
+        let mut file = File::open("todos.json")?;
+        file.read_to_string(&mut todos_str)?;
+        let mut todos: Vec<Todo> = serde_json::from_str(&todos_str)?;
+
+        todos.sort_by(|a, b| {
+            a.created_at()
+                .timestamp_millis()
+                .cmp(&b.created_at().timestamp_millis())
+        });
+
+        Ok(Self { todos })
+    }
+
+    /// Saves the todos to "todos.json"
+    pub fn save(&self) {
+        let todos_str = serde_json::to_string(&self.todos).unwrap();
+        File::create("todos.json")
+            .unwrap()
+            .write_all(todos_str.as_bytes())
+            .unwrap();
+    }
+
+    /// List todos
+    pub fn list(&self) {
+        for todo in &self.todos {
+            println!("#{}: {}", todo.id(), todo.title());
+        }
+    }
+
+    pub fn todos_mut(&mut self) -> &mut Vec<Todo> {
+        &mut self.todos
+    }
+}
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Todo {
